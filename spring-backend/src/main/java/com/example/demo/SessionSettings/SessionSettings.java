d7ec2a0ecaf4fd782aaee361d236c28d7ee62b14
mahmudulhasanyeamim <mahmudulyeamim@gmail.com>
2025-01-03 19:28:52 +0600

    getters setters added

--- spring-backend/src/main/java/com/example/demo/SessionSettings/SessionSettings.java
+++ spring-backend/src/main/java/com/example/demo/SessionSettings/SessionSettings.java
@@ -11,18 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @Entity
 @Builder
 public class SessionSettings {
@@ -59,6 +51,102 @@ public class SessionSettings {
     private boolean canViewCurrentSessionParticipants = false;
     @Builder.Default
     private boolean displaySessionStatistics = true;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public String getSessionBanner() {
+        return sessionBanner;
+    }
+
+    public void setSessionBanner(String sessionBanner) {
+        this.sessionBanner = sessionBanner;
+    }
+
+    public String getAboutSessionOwner() {
+        return aboutSessionOwner;
+    }
+
+    public void setAboutSessionOwner(String aboutSessionOwner) {
+        this.aboutSessionOwner = aboutSessionOwner;
+    }
+
+    public ScheduledSessionBehavior getScheduledSessionBehavior() {
+        return scheduledSessionBehavior;
+    }
+
+    public void setScheduledSessionBehavior(ScheduledSessionBehavior scheduledSessionBehavior) {
+        this.scheduledSessionBehavior = scheduledSessionBehavior;
+    }
+
+    public Long getNotifyScheduledSessionBefore() {
+        return notifyScheduledSessionBefore;
+    }
+
+    public void setNotifyScheduledSessionBefore(Long notifyScheduledSessionBefore) {
+        this.notifyScheduledSessionBefore = notifyScheduledSessionBefore;
+    }
+
+    public boolean isNotifyThroughEmail() {
+        return notifyThroughEmail;
+    }
+
+    public void setNotifyThroughEmail(boolean notifyThroughEmail) {
+        this.notifyThroughEmail = notifyThroughEmail;
+    }
+
+    public int getMaximumAllowedParticipantsPerRoom() {
+        return maximumAllowedParticipantsPerRoom;
+    }
+
+    public void setMaximumAllowedParticipantsPerRoom(int maximumAllowedParticipantsPerRoom) {
+        this.maximumAllowedParticipantsPerRoom = maximumAllowedParticipantsPerRoom;
+    }
+
+    public boolean isCanViewWaitingList() {
+        return canViewWaitingList;
+    }
+
+    public void setCanViewWaitingList(boolean canViewWaitingList) {
+        this.canViewWaitingList = canViewWaitingList;
+    }
+
+    public boolean isCanViewParticipantsList() {
+        return canViewParticipantsList;
+    }
+
+    public void setCanViewParticipantsList(boolean canViewParticipantsList) {
+        this.canViewParticipantsList = canViewParticipantsList;
+    }
+
+    public boolean isCanViewCurrentSessionParticipants() {
+        return canViewCurrentSessionParticipants;
+    }
+
+    public void setCanViewCurrentSessionParticipants(boolean canViewCurrentSessionParticipants) {
+        this.canViewCurrentSessionParticipants = canViewCurrentSessionParticipants;
+    }
+
+    public boolean isDisplaySessionStatistics() {
+        return displaySessionStatistics;
+    }
+
+    public void setDisplaySessionStatistics(boolean displaySessionStatistics) {
+        this.displaySessionStatistics = displaySessionStatistics;
+    }
 }
 
 enum ScheduledSessionBehavior {
